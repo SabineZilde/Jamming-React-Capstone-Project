60688e45be7df37af738fbf83a9568c44c993f6f
SabineZilde <sabine.zilde@protonmail.ch>
2021-09-30 09:14:31 +0300

    Add and remove buttons

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,33 +22,49 @@ class App extends React.Component {
             ]
         };
         this.addTrack = this.addTrack.bind(this);
-    }
+        this.removeTrack = this.removeTrack.bind(this);
+        this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    };
 
     addTrack(track) {
         let tracks = this.state.playlistTracks;
         if (tracks.find(savedTrack => savedTrack.id === track.id)) {
             return;
-        }
+        };
 
         tracks.push(track);
-        this.setState({playlistTracks: tracks});
-    }
+        this.setState({ playlistTracks: tracks });
+    };
 
-    render() {
-        return (
-            <div>
-                <h1>Ja<span className="highlight">mmm</span>ing</h1>
-                <div class="App">
-                    <SearchBar />
-                    <div className="App-playlist">
-                        <SearchResults searchResults={this.state.searchResults} 
-                                        onAdd={this.addTrack} />
-                        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
-                    </div>
+    removeTrack(track) {
+        let tracks = this.state.playlistTracks;
+        tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+        this.setState({ plaulistTracks: tracks });
+    };
+
+    updatePlaylistName(name) {
+        this.setState({ playlistName: name });
+    };
+
+render() {
+    return (
+        <div>
+            <h1>Ja<span className="highlight">mmm</span>ing</h1>
+            <div className="App">
+                <SearchBar />
+                <div className="App-playlist">
+                    <SearchResults searchResults={this.state.searchResults}
+                        onAdd={this.addTrack} />
+                    <Playlist playlistName={this.state.playlistName}
+                        playlistTracks={this.state.playlistTracks}
+                        onRemove={this.removeTrack} 
+                        onNameChange={this.updatePlaylistName} />
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    )
+};
+};
 
 export default App;
